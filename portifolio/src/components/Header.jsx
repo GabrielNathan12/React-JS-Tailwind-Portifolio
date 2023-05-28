7b68114c7f311ec364496f1eeaf3ea2eb50ba7e5
Gabriel Nathan Almeida Silva Gabriel <76185909+GabrielNathan12@users.noreply.github.com>
2023-05-28 17:52:40 -0300

    feat:criacao do projeto

---
+++ portifolio/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from '../assets/logo.png';
+
+const Header = () =>{
+    return <header className="py-8 ">
+        <div className="container mx-auto">
+            <div className="flex justify-between items-center">
+                {/* Logo */ }
+                <a href="#">
+                    <img src={logo} alt=""/>
+                </a>
+                <button className="btn btn-sm">Meus Trabalhos</button>
+            </div>
+        </div>
+    </header>
+}
+
+export default Header;
